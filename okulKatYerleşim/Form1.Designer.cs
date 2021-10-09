136c663c816540d7cbaf1bf42358fee9aa6df105
onurc <onurc@LAPTOP-7342543N>
2021-10-09 16:37:11 +0300

    Add project files.

---
+++ okulKatYerleşim/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace okulKatYerleşim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKat = new System.Windows.Forms.TextBox();
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.lstKat = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(166, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Okul Kat Yerleşim Planı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kat Numarası:";
+            // 
+            // txtKat
+            // 
+            this.txtKat.Location = new System.Drawing.Point(169, 138);
+            this.txtKat.Name = "txtKat";
+            this.txtKat.Size = new System.Drawing.Size(100, 22);
+            this.txtKat.TabIndex = 2;
+            // 
+            // btnGoster
+            // 
+            this.btnGoster.Location = new System.Drawing.Point(319, 132);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(75, 35);
+            this.btnGoster.TabIndex = 3;
+            this.btnGoster.Text = "Göster";
+            this.btnGoster.UseVisualStyleBackColor = true;
+            this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
+            // 
+            // lstKat
+            // 
+            this.lstKat.FormattingEnabled = true;
+            this.lstKat.ItemHeight = 16;
+            this.lstKat.Location = new System.Drawing.Point(53, 236);
+            this.lstKat.Name = "lstKat";
+            this.lstKat.Size = new System.Drawing.Size(341, 116);
+            this.lstKat.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 395);
+            this.Controls.Add(this.lstKat);
+            this.Controls.Add(this.btnGoster);
+            this.Controls.Add(this.txtKat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKat;
+        private System.Windows.Forms.Button btnGoster;
+        private System.Windows.Forms.ListBox lstKat;
+    }
+}
+
---
+++ okulKatYerleşim/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace okulKatYerleşim
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGoster_Click(object sender, EventArgs e)
+        {
+            int k1, k2, k3;
+            
+            k1 = Convert.ToInt32(txtKat.Text);
+            txtKat.Text = (k1.ToString());
+            k2 = Convert.ToInt32(txtKat.Text);
+            txtKat.Text = (k2.ToString());
+            k3 = Convert.ToInt32(txtKat.Text);
+            txtKat.Text = (k3.ToString());
+            if (k1==1)
+            {
+                lstKat.Items.Add("9A-Sınıfı");
+                lstKat.Items.Add("9B-Sınıfı");
+                lstKat.Items.Add("9C-Sınıfı");
+                lstKat.Items.Add("9D-Sınıfı");                
+            }
+            
+            else if (k2==2)
+            {
+                lstKat.Items.Add("10A-Sınıfı");
+                lstKat.Items.Add("10B-Sınıfı");
+                lstKat.Items.Add("10C-Sınıfı");
+                lstKat.Items.Add("10D-Sınıfı");                
+            }
+            else if(k3==3)
+            {
+                lstKat.Items.Add("11A-Sınıfı");
+                lstKat.Items.Add("11B-Sınıfı");
+                lstKat.Items.Add("11C-Sınıfı");
+                lstKat.Items.Add("11D-Sınıfı");
+            }
+            
+            
+        }
+    }
+}
